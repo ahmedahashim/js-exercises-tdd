0d85fe9bd832a03051b8034bce2de0b9ef2e355f
ahmed <ahmed.almadina@hotmail.com>
2019-02-21 22:22:51 +0000

    implement- Kata-fizz-buzz

--- III.tdd-katas/fizz-buzz/fizz-buzz.js
+++ III.tdd-katas/fizz-buzz/fizz-buzz.js
@@ -1,23 +1,43 @@
-module.exports =function fizzbuzz(number){
+// function fizzbuzz(number){
    
 
-    for (i = 1; i <= 100; i++) {
-        // if the number is multplie by 3 & 5, write "FizzBuzz"
+//     for (i = 1; i <= 100; i++) {
+//         // if the number is multplie by 3 & 5, write "FizzBuzz"
        
-        if (i % 15=== 0) {
-            return "FizzBuzz";
-        }
-        // if the number is multiple by 3, write "Fizz"
-        else if (i % 3 === 0) {
-           return"Fizz";
-        }
-        // if the number is multiple by 5, write "Buzz
-        else if (i % 5 === 0) {
-           return"Buzz";
-        }
-        // otherwise, write just the number
-        else {
-           return number;
-        }
-    }
+//         if (i % 15=== 0) {
+//             return "FizzBuzz";
+//         }
+//         // if the number is multiple by 3, write "Fizz"
+//         else if (i % 3 === 0) {
+//            return"Fizz" ;
+//         }
+//         // if the number is multiple by 5, write "Buzz
+//         else if (i % 5 === 0) {
+//            return"Buzz";
+//         }
+//         // otherwise, write just the number
+//         else {
+//            return number
+//         }
+//     }
+// }
+function fizzBuzz(i) {
+    
+    if (i % 15 == 0)
+        return 'FizzBuzz';
+    else if (i % 5 == 0)
+        return 'Buzz';
+    else if (i % 3 == 0)
+        return 'Fizz';
+    else
+        return i;
 }
+
+// Return an array
+function fizzbuzz(number) {
+    var array = [];
+    for (var i = 1; i <= number; ++i) array.push(fizzBuzz(i));
+    return array.toString();
+}
+
+module.exports = fizzbuzz;
--- III.tdd-katas/fizz-buzz/fizz-buzz.test.js
+++ III.tdd-katas/fizz-buzz/fizz-buzz.test.js
@@ -2,10 +2,14 @@ var fizzbuzz = require('./fizz-buzz')
 
 test('prints 1', function () {
     var result = fizzbuzz(1)
-    expect(result).toEqual(1)
+    expect(result).toEqual('1')
 })
 
-test('prints 2', function () {
+test('prints 1,2', function () {
     var result = fizzbuzz(2)
-    expect(result).toEqual(2)
+    expect(result).toEqual('1,2')
+})
+test('prints 1,2,Fizz', function () {
+    var result = fizzbuzz(3)
+    expect(result).toEqual('1,2,Fizz')
 })
